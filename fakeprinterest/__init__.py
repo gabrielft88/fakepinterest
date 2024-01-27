357b0740af6047316acc32c74f455aa4709e4a1e
unknown <gabrielfariateixeira@gmail.com>
2024-01-27 14:44:50 -0300

    Mudança banco postgresql

--- fakeprinterest/__init__.py
+++ fakeprinterest/__init__.py
@@ -2,9 +2,10 @@ from flask import  Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "3b54c89f7bd3bc32624a275c0d2fbcd8"
 app.config["UPLOAD_FOLDER"] = "static/fotos_post"
 
